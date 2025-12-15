92450a981adea5294c4181293316ae8f31cf25f8
Юрий <amirovurij@gmail.com>
2025-12-15 22:11:28 +0300

    Add files via upload

---
+++ 10/10/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Program
+{
+    static bool Good(int prev, int curr, int m)
+    {
+        for (int j = 0; j < m - 1; j++)
+        {
+            int a = (prev >> j) & 1;
+            int b = (prev >> (j + 1)) & 1;
+            int c = (curr >> j) & 1;
+            int d = (curr >> (j + 1)) & 1;
+            if (a == b && b == c && c == d)
+                return false;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+
+        if (n < m)
+        {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+
+        if (n == 1)
+        {
+            Console.WriteLine(1L << m);
+            return;
+        }
+
+        int masks = 1 << m;
+        long[,] dp = new long[masks, masks];
+
+        for (int prev = 0; prev < masks; prev++)
+        {
+            for (int curr = 0; curr < masks; curr++)
+            {
+                if (Good(prev, curr, m))
+                    dp[prev, curr] = 1;
+            }
+        }
+
+        for (int i = 3; i <= n; i++)
+        {
+            long[,] next = new long[masks, masks];
+            for (int prev = 0; prev < masks; prev++)
+            {
+                for (int curr = 0; curr < masks; curr++)
+                {
+                    if (dp[prev, curr] == 0) continue;
+                    for (int nxt = 0; nxt < masks; nxt++)
+                    {
+                        if (Good(curr, nxt, m))
+                            next[curr, nxt] += dp[prev, curr];
+                    }
+                }
+            }
+            dp = next;
+        }
+
+        long answer = 0;
+        for (int prev = 0; prev < masks; prev++)
+            for (int curr = 0; curr < masks; curr++)
+                answer += dp[prev, curr];
+
+        Console.WriteLine(answer);
+    }
+}
---
+++ 9/9/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    static int n, m;
+    static int[] blocked;
+    static long[,] dp;
+    static int fullMask;
+
+    static void Rec(int row, int col, int prevMask, int curMask, int nextMask, long val)
+    {
+        if (col == m)
+        {
+            if (((prevMask | curMask) & (~blocked[row])) == (~blocked[row] & fullMask))
+                dp[row + 1, nextMask] += val;
+            return;
+        }
+
+        int bit = 1 << col;
+        if ((prevMask & bit) != 0)
+        {
+            Rec(row, col + 1, prevMask, curMask, nextMask, val);
+            return;
+        }
+
+        if ((blocked[row] & bit) != 0)
+        {
+            Rec(row, col + 1, prevMask, curMask, nextMask, val);
+            return;
+        }
+
+        if (col + 1 < m && (prevMask & (1 << (col + 1))) == 0 && (blocked[row] & (1 << (col + 1))) == 0)
+        {
+            int newCur = curMask | bit | (1 << (col + 1));
+            Rec(row, col + 2, prevMask, newCur, nextMask, val);
+        }
+
+        if (row + 1 < n && (blocked[row + 1] & bit) == 0)
+        {
+            int newCur = curMask | bit;
+            int newNext = nextMask | bit;
+            Rec(row, col + 1, prevMask, newCur, newNext, val);
+        }
+    }
+
+    static void Main()
+    {
+        string[] nm = Console.ReadLine().Split();
+        n = int.Parse(nm[0]);
+        m = int.Parse(nm[1]);
+
+        blocked = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            string s = Console.ReadLine();
+            int mask = 0;
+            for (int j = 0; j < m; j++)
+                if (s[j] == 'X')
+                    mask |= 1 << j;
+            blocked[i] = mask;
+        }
+
+        fullMask = (1 << m) - 1;
+        dp = new long[n + 1, 1 << m];
+        dp[0, 0] = 1;
+
+        for (int row = 0; row < n; row++)
+        {
+            for (int mask = 0; mask <= fullMask; mask++)
+            {
+                if (dp[row, mask] != 0)
+                    Rec(row, 0, mask, 0, 0, dp[row, mask]);
+            }
+        }
+
+        Console.WriteLine(dp[n, 0]);
+    }
+}
