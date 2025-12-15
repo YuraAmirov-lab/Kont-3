59df7983ad3028fd21742abb419bef77d1d076eb
Юрий <amirovurij@gmail.com>
2025-12-15 22:10:31 +0300

    Add files via upload

---
+++ 4/4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int mod = 1000000000;
+        int n = int.Parse(Console.ReadLine());
+
+        long[,] dp = new long[n + 1, 10];
+
+        for (int d = 0; d < 10; d++)
+        {
+            if (d != 0 && d != 8)
+                dp[1, d] = 1;
+        }
+
+        for (int len = 2; len <= n; len++)
+        {
+            for (int d = 0; d < 10; d++)
+            {
+                long sum = 0;
+                switch (d)
+                {
+                    case 0: sum = dp[len - 1, 4] + dp[len - 1, 6]; break;
+                    case 1: sum = dp[len - 1, 6] + dp[len - 1, 8]; break;
+                    case 2: sum = dp[len - 1, 7] + dp[len - 1, 9]; break;
+                    case 3: sum = dp[len - 1, 4] + dp[len - 1, 8]; break;
+                    case 4: sum = dp[len - 1, 3] + dp[len - 1, 9] + dp[len - 1, 0]; break;
+                    case 5: sum = 0; break;
+                    case 6: sum = dp[len - 1, 1] + dp[len - 1, 7] + dp[len - 1, 0]; break;
+                    case 7: sum = dp[len - 1, 2] + dp[len - 1, 6]; break;
+                    case 8: sum = dp[len - 1, 1] + dp[len - 1, 3]; break;
+                    case 9: sum = dp[len - 1, 2] + dp[len - 1, 4]; break;
+                }
+                dp[len, d] = sum % mod;
+            }
+        }
+
+        long answer = 0;
+        for (int d = 0; d < 10; d++)
+        {
+            answer += dp[n, d];
+            answer %= mod;
+        }
+
+        Console.WriteLine(answer);
+    }
+}
---
+++ 5/5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s1 = Console.ReadLine();
+        string s2 = Console.ReadLine();
+        int n = s1.Length;
+        int m = s2.Length;
+
+        int[,] dp = new int[n + 1, m + 1];
+
+        for (int i = 0; i <= n; i++) dp[i, 0] = i;
+        for (int j = 0; j <= m; j++) dp[0, j] = j;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                int cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+                dp[i, j] = Math.Min(Math.Min(dp[i - 1, j] + 1, dp[i, j - 1] + 1), dp[i - 1, j - 1] + cost);
+            }
+        }
+
+        Console.WriteLine(dp[n, m]);
+    }
+}
